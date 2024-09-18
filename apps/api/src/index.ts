b69c420a795a1a965804814bd8d1c843315999e3
Imam Alfarizi <imam.alfarizi.777@gmail.com>
2024-09-18 20:50:10 +0700

    update

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,6 @@
+import app from './app.js';
 import { env } from './config/env.js';
-import app from './server.js';
 
-async function main() {
-  try {
-    app.listen({ port: env.PORT });
-    console.log(`Server ready at http://localhost:${env.PORT}`);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-}
-
-main();
+app.listen(env.PORT, () => {
+  console.log(`Listening: http://localhost:${env.PORT}`);
+});
